5e90c126456ed0048bf28937fc8a7aa93d7ee89e
TMGilchrist <telcontar@hotmail.co.uk>
2017-10-18 12:47:35 +0100

    Updated main.py and sprite.py
    Requires file structure to work correctly
    
    Directory
        - SpriteComponents
            - body
            - head
        - NewSprites

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pygame
+import glob
+import random
 from sprite import sprite
 
-#Load images for sprite components. Images should be loaded into lists or arrays for each component
-body = pygame.image.load('spritePlaceHolder1.jpeg')
-head = pygame.image.load('spritePlaceHolder2.jpeg')
 
-#Create new sprite with selected components. Components should be taken randomly from list
-newSprite = sprite(body, head, 0, 0)
-newSprite.draw((0, 0), (0, 0), 'jpg')
+body = []
+head = []
+
+#Specify file type to load and the image directories
+spriteFileType = "jpeg"
+bodyPath = glob.glob("SpriteComponents/body/*" + spriteFileType)
+headPath = glob.glob("SpriteComponents/head/*" + spriteFileType)
+
+#Adds all image files in path to lists
+for filename in bodyPath:
+    body.append(pygame.image.load(filename))
+
+for filename in headPath:
+    head.append(pygame.image.load(filename))
+
+
+
+# Create new sprite with random component
+
+# Arguments: Sprite Size (total canvas), bodyImage, headImage, feetImage, weaponImage
+newSprite = sprite((100, 100), random.choice(body), random.choice(head), 0, 0)
+
+# Arguments: bodyPos, headPos, fileType
+newSprite.draw((40, 40), (60, 40), 'png')
--- sprite.py
+++ sprite.py
@@ -1,21 +1,25 @@
 import pygame
 
+
 class sprite:
 
-    #Initalise new sprite with component images
-    def __init__(self, body, head, feet, weapon):
+    # Initialise new sprite with component images
+    def __init__(self, size, body, head, feet, weapon):
         self.body = body
         self.head = head
         self.feet = feet
         self.weapon = weapon
+        self.size = size
 
 
-    #Draw component images onto a base surface then save the surface as a single sprite
+# Draw component images onto a base surface then save the surface as a single sprite
     def draw(self, bodyPos, headPos, fileType):
-        spriteBase = pygame.Surface((800, 600))
+        spriteBase = pygame.Surface(self.size, pygame.SRCALPHA, 32)
+
+        spriteBase.set_alpha(255)
 
         spriteBase.blit(self.body, bodyPos)
         spriteBase.blit(self.head, headPos)
 
         # Save sprite
-        pygame.image.save(spriteBase, 'sprite.' + fileType)
+        pygame.image.save(spriteBase, 'NewSprites/sprite.' + fileType)

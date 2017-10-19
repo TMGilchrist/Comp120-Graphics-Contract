44bd985645dfc895420229ff28b2a024aea95b11
TMGilchrist <telcontar@hotmail.co.uk>
2017-10-19 13:04:31 +0100

    Updated main.py, sprite.py and added spriteNames
    Added seperate save function for sprite.py
    Added text file of currently used sprite names

--- main.py
+++ main.py
@@ -3,28 +3,42 @@ import glob
 import random
 from sprite import sprite
 
-
+base = []
+legs = []
 body = []
 head = []
 
 #Specify file type to load and the image directories
-spriteFileType = "jpeg"
-bodyPath = glob.glob("SpriteComponents/body/*" + spriteFileType)
-headPath = glob.glob("SpriteComponents/head/*" + spriteFileType)
+spriteFileType = ".png"
+
+pathToAssets = "D:/Uni/Programming/HereBeDragons/Assets"
+
+basePath = glob.glob(pathToAssets + "/Sprites/base/*" + spriteFileType)
+legsPath = glob.glob(pathToAssets + "/Sprites/legs/*" + spriteFileType)
+bodyPath = glob.glob(pathToAssets + "/Sprites/body/*" + spriteFileType)
+headPath = glob.glob(pathToAssets + "/Sprites/hair/*" + spriteFileType)
+
+print("got paths")
 
 #Adds all image files in path to lists
+for filename in basePath:
+    base.append(pygame.image.load(filename))
+
+for filename in legsPath:
+    legs.append(pygame.image.load(filename))
+
 for filename in bodyPath:
     body.append(pygame.image.load(filename))
 
 for filename in headPath:
     head.append(pygame.image.load(filename))
 
-
+print("added sprites")
 
 # Create new sprite with random component
 
-# Arguments: Sprite Size (total canvas), bodyImage, headImage, feetImage, weaponImage
-newSprite = sprite((100, 100), random.choice(body), random.choice(head), 0, 0)
+# Arguments: Sprite Size (total canvas), baseImage bodyImage, headImage, feetImage, weaponImage
+newSprite = sprite((16, 16), random.choice(base), random.choice(legs), random.choice(body), random.choice(head), 0, 0)
 
 # Arguments: bodyPos, headPos, fileType
-newSprite.draw((40, 40), (60, 40), 'png')
+newSprite.draw('png')
--- sprite.py
+++ sprite.py
@@ -4,22 +4,57 @@ import pygame
 class sprite:
 
     # Initialise new sprite with component images
-    def __init__(self, size, body, head, feet, weapon):
+    def __init__(self, size, base, legs, body, head, feet, weapon):
+        self.size = size
+        self.base = base
+        self.legs = legs
         self.body = body
         self.head = head
         self.feet = feet
         self.weapon = weapon
-        self.size = size
+
+
+
+    # Draw component images onto a base surface then save the surface as a single sprite
+    def draw(self, fileType):
+        spriteBase = pygame.Surface(self.size, pygame.SRCALPHA, 32)
+        spriteBase.set_alpha(255)
+
+        spriteBase.blit(self.base, (0, 0))
+        spriteBase.blit(self.legs, (0, 0))
+        spriteBase.blit(self.body, (0, 0))
+        spriteBase.blit(self.head, (0, 0))
+
+        # Save sprite
+        #pygame.image.save(spriteBase, 'NewSprites/sprite.' + fileType)
+        self.saveWithID(spriteBase, fileType)
 
 
 # Draw component images onto a base surface then save the surface as a single sprite
-    def draw(self, bodyPos, headPos, fileType):
+    def drawWithPosition(self, basePos, bodyPos, legsPos, headPos, fileType):
         spriteBase = pygame.Surface(self.size, pygame.SRCALPHA, 32)
-
         spriteBase.set_alpha(255)
 
+        spriteBase.blit(self.base, basePos)
+        spriteBase.blit(self.legs, legsPos)
         spriteBase.blit(self.body, bodyPos)
         spriteBase.blit(self.head, headPos)
 
         # Save sprite
         pygame.image.save(spriteBase, 'NewSprites/sprite.' + fileType)
+
+
+
+# Save sprite with incrementing filename based on a .txt file of existing names
+    def saveWithID(self, toSave, fileType):
+
+        spriteID = 0
+
+        with open("spriteNames.txt", "a+") as f:
+            for line in f:
+                spriteID += 1
+
+            f.write("sprite" + str(spriteID) + "\n")
+            f.close()
+
+        pygame.image.save(toSave, "NewSprites/sprite" + str(spriteID) + "." + fileType)
